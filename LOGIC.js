eb64d97a9197a9242a400406cfa58aee808c56fb
AmamiSQ <r_zimmerman@outlook.com>
2022-10-07 09:26:07 +0200

    final

--- LOGIC.js
+++ LOGIC.js
@@ -4,11 +4,27 @@ function input(num){
 
 }
 
+function remAll(){
+    form1.result.value='';
+}
+
 function backspace(){
-    form1.result.value+='';
+    var temp = form1.result.value;
+    form1.result.value = temp.substring(0, temp.length-1);
+}
+
+function hehe(){
+    alert("Please click OK to continue");
+
+    if(window.confirm("Almost there...")){
+        window.location.href='https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran';
+    }
 }
 
 
 function equal(){
-
+    var check = form1.result.value;
+    if(check){
+        form1.result.value = eval(form1.result.value);
+    }
 }
